3fe54ba0b4d3db4eea718300e0fbbad321afee34
Sebastian Asis <sasisromero10@gmail.com>
2021-03-15 13:44:07 -0500

    :fire: NEW: api-simulated

--- src/components/pages/Classes.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const Classes = () => {
-  return (
-    <div className = "ed-grid">
-      <h1>Classes</h1>
-    </div>
-  )
-}
--- src/components/pages/Page404.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const Page404 = () => {
-  return (
-    <div className = "ed-grid">
-      <h1>Page404</h1>
-    </div>
-  )
-}
